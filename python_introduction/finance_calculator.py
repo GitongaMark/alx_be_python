67cb114152babd3dc1cb04b831762b29222f2eec
MARK GITONGA <149807712+GitongaMark@users.noreply.github.com>
2025-07-12 17:24:18 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-Monthly_savings = income - expenses
+Monthly_savings = monthly_income - monthly_expenses
 print(f"Your monthly savings are: ${Monthly_savings}")
 
 interest = 0.05
